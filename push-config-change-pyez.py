da6fb571799ccc70a33a020014b15e5ee3597b96
fernandescris <41203910+fernandescris@users.noreply.github.com>
2018-08-07 15:29:43 -0400

    Add files via upload

--- push-config-change-pyez.py
+++ push-config-change-pyez.py
@@ -1,8 +1,7 @@
 from jnpr.junos import Device
-dev = Device('192.168.0.1')
+dev = Device(host = '172.19.64.26' ,  user='csim', password='Juniper123' )
 from jnpr.junos.utils.config import Config
 cu = Config(dev)
 dev.open()
 rsp = cu.load( path="config-example.conf" )
 cu.commit()
-
